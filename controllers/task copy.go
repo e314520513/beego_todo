9bdff31111453897ae4a220bf025225536f99a2a
wahaha <e314520513@gmail.com>
2021-10-24 16:30:42 +0800

    update

--- controllers/task copy.go
+++ controllers/task copy.go
@@ -1,4 +1,4 @@
-package controllers
+package bk
 
 import(
 	"encoding/json"
--- controllers/task.go
+++ controllers/task.go
@@ -1,11 +1,10 @@
 package controllers
 
-import (
+import(
 	"encoding/json"
 	"strconv"
-
 	"github.com/astaxie/beego"
-	"github.com/beego/samples/todo/models"
+	"beego_todo/models"
 )
 
 type TaskController struct {
@@ -44,6 +43,22 @@ func (this *TaskController) NewTask() {
 		this.Ctx.Output.SetStatus(400)
 		this.Ctx.Output.Body([]byte(err.Error()))
 		return
+	}
+	models.DefaultTaskList.Save(t)
+}
+
+func (this *TaskController) NewTask(){
+	req  := struct{Title string}{}
+	if err:= json.Unmarshal(this.Ctx.Input.RequestBody,&req); err != nil {
+		this.Ctx.Output.SetStatus(4000)
+		this.Ctx.Output.Body([]byte("empty title"))
+		return
+	}
+	t,err := models.NewTask(req.Title)
+	if err != nil {
+		this.Ctx.Output.SetStatus(400)
+		this.Ctx.Output.Body([]byte(err.Error()))
+		return 
 	}
 	models.DefaultTaskList.Save(t)
 }
