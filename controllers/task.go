2711a0ac49cf10d9581a995b9228ca0e4217a635
wahaha <e314520513@gmail.com>
2021-10-24 15:35:10 +0800

    update

--- controllers/task.go
+++ controllers/task.go
@@ -1,6 +1,7 @@
 package controllers
 
 import(
+	"encoding/json"
 	"github.com/astaxie/beego"
 	"beego_todo/models"
 )
@@ -13,5 +14,21 @@ func (this *TaskController) ListTasks(){
 	res := struct{tasks []*models.Task}{models.DefaultTaskList.All()}
 	this.Data["json"] = res
 	this.ServeJSON()
+}
+
+func (this *TaskController) NewTask(){
+	req  := struct{Title string}{}
+	if err:= json.Unmarshal(this.Ctx.Input.RequestBody,&req); err != nil {
+		this.Ctx.Output.SetStatus(4000)
+		this.Ctx.Output.Body([]byte("empty title"))
+		return
+	}
+	t,err := models.NewTask(req.Title)
+	if err != nil {
+		this.Ctx.Output.SetStatus(400)
+		this.Ctx.Output.Body([]byte(err.Error()))
+		return 
+	}
+	models.DefaultTaskList.Save(t)
 }
 
