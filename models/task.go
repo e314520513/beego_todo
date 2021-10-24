211c4858f72daeaa11aab44dd6e6f89e24013ee9
wahaha <e314520513@gmail.com>
2021-10-24 16:18:35 +0800

    update

--- models/task.go
+++ models/task.go
@@ -1,67 +1,76 @@
 package models
 
-import(
+import (
 	"fmt"
 )
 
 var DefaultTaskList *TaskManager
 
-type Task struct{
-	ID int64
-	Title string
-	Done bool
+type Task struct {
+	ID    int64  // Unique identifier
+	Title string // Description
+	Done  bool   // Is this task done?
 }
 
-type TaskManager struct{
-	tasks []*Task
-	lastID int64
-}
-func NewTask(title string)(*Task,error){
-	if title == ""{
+// NewTask creates a new task given a title, that can't be empty.
+func NewTask(title string) (*Task, error) {
+	if title == "" {
 		return nil, fmt.Errorf("empty title")
 	}
-	return &Task{0,title,false},nil
-
-}
-func (this *TaskManager) Save(task *Task) error{
-	if task.ID == 0 {
-		this.lastID++
-		task.ID = this.lastID
-	    this.tasks = append(this.tasks,cloneTask(task))
-		return nil
-	}
-	for i,t :=range this.tasks{
-		if t.ID==task.ID{
-			this.tasks[i] = cloneTask(task)
-			return nil
-		}
-	}
-
-	return fmt.Errorf("unknown task")
+	return &Task{0, title, false}, nil
 }
 
-func cloneTask(t *Task) *Task{
-	c := *t
-	return  &c
+// TaskManager manages a list of tasks in memory.
+type TaskManager struct {
+	tasks  []*Task
+	lastID int64
 }
-func NewTaskManager() *TaskManager{
+
+// NewTaskManager returns an empty TaskManager.
+func NewTaskManager() *TaskManager {
 	return &TaskManager{}
 }
 
-func (this *TaskManager) All() []*Task{
-	return this.tasks
-}
+// Save saves the given Task in the TaskManager.
+func (m *TaskManager) Save(task *Task) error {
+	if task.ID == 0 {
+		m.lastID++
+		task.ID = m.lastID
+		m.tasks = append(m.tasks, cloneTask(task))
+		return nil
+	}
 
-func (this * TaskManager) Find(ID int64)(*Task, bool){
-	for _,t := range this.tasks{
-		if t.ID == ID {
-			return t,true
+	for i, t := range m.tasks {
+		if t.ID == task.ID {
+			m.tasks[i] = cloneTask(task)
+			return nil
 		}
 	}
-	return nil,false
+	return fmt.Errorf("unknown task")
 }
 
+// cloneTask creates and returns a deep copy of the given Task.
+func cloneTask(t *Task) *Task {
+	c := *t
+	return &c
+}
 
-func init(){
+// All returns the list of all the Tasks in the TaskManager.
+func (m *TaskManager) All() []*Task {
+	return m.tasks
+}
+
+// Find returns the Task with the given id in the TaskManager and a boolean
+// indicating if the id was found.
+func (m *TaskManager) Find(ID int64) (*Task, bool) {
+	for _, t := range m.tasks {
+		if t.ID == ID {
+			return t, true
+		}
+	}
+	return nil, false
+}
+
+func init() {
 	DefaultTaskList = NewTaskManager()
 }
