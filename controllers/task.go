4d62fa8950698c3815116b4d64aab8756f83273f
wahaha <e314520513@gmail.com>
2021-10-24 15:42:22 +0800

    update

--- controllers/task.go
+++ controllers/task.go
@@ -32,3 +32,47 @@ func (this *TaskController) NewTask(){
 	models.DefaultTaskList.Save(t)
 }
 
+func (this *TaskController) GetTask() {
+	id := this.Ctx.Input.Param(":id")
+	beego.Info("Task is ", id)
+	intid, _ := strconv.ParseInt(id, 10, 64)
+	t, ok := models.DefaultTaskList.Find(intid)
+	beego.Info("Found", ok)
+	if !ok {
+		this.Ctx.Output.SetStatus(404)
+		this.Ctx.Output.Body([]byte("task not found"))
+		return
+	}
+	this.Data["json"] = t
+	this.ServeJSON()
+}
+
+// Example:
+//
+//   req: PUT /task/1 {"ID": 1, "Title": "Learn Go", "Done": true}
+//   res: 200
+//
+//   req: PUT /task/2 {"ID": 2, "Title": "Learn Go", "Done": true}
+//   res: 400 inconsistent task IDs
+func (this *TaskController) UpdateTask() {
+	id := this.Ctx.Input.Param(":id")
+	beego.Info("Task is ", id)
+	intid, _ := strconv.ParseInt(id, 10, 64)
+	var t models.Task
+	if err := json.Unmarshal(this.Ctx.Input.RequestBody, &t); err != nil {
+		this.Ctx.Output.SetStatus(400)
+		this.Ctx.Output.Body([]byte(err.Error()))
+		return
+	}
+	if t.ID != intid {
+		this.Ctx.Output.SetStatus(400)
+		this.Ctx.Output.Body([]byte("inconsistent task IDs"))
+		return
+	}
+	if _, ok := models.DefaultTaskList.Find(intid); !ok {
+		this.Ctx.Output.SetStatus(400)
+		this.Ctx.Output.Body([]byte("task not found"))
+		return
+	}
+	models.DefaultTaskList.Save(&t)
+}
